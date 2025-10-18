920a862d632cf3f1ae6df216ef3719f7ce3e5719
14Memansalman <stud.2201201080@kpgu.ac.in>
2025-10-18 18:23:15 +0530

    add 1

--- admin_ScanNServe/src/App.jsx
+++ admin_ScanNServe/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://scannserve-web-backend.onrender.com"
 
   return (
     <div>
