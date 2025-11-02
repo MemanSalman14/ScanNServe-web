99120a5e03a55ce8e224cd9318e384c935dc2c09
14Memansalman <stud.2201201080@kpgu.ac.in>
2025-11-02 12:21:40 +0530

    solve error 1

--- backend_ScanNServe/server.js
+++ backend_ScanNServe/server.js
@@ -13,8 +13,17 @@ const app = express()
 const port = process.env.PORT || 4000
 
 // middleware
+
+{/*
 app.use(express.json())
 app.use(cors())
+*/}
+
+app.use(express.json({ limit: '10mb' })) // Increase payload size limit
+app.use(cors({
+  origin: 'https://scan-n-serve-frontend.vercel.app', // Allow frontend domain
+  credentials: true
+}))
 
 // db connection
 connectDB()
