264894bedfe802030a2f88644718ad072dc1ac13
14Memansalman <stud.2201201080@kpgu.ac.in>
2025-12-12 15:54:23 +0530

    add 4

--- backend_ScanNServe/server.js
+++ backend_ScanNServe/server.js
@@ -34,6 +34,8 @@ app.use(cors())
 */}
 
 app.use(express.json({ limit: '10mb' })) // Increase payload size limit
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(cors({
   origin: [
     'http://localhost:5173',
