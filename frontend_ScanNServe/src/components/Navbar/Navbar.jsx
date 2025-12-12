d7455588a1865d867de52eff28b3f8902703dc11
14Memansalman <stud.2201201080@kpgu.ac.in>
2025-12-13 00:26:10 +0530

    fix design

--- frontend_ScanNServe/src/components/Navbar/Navbar.jsx
+++ frontend_ScanNServe/src/components/Navbar/Navbar.jsx
@@ -12,17 +12,17 @@ const Navbar = ({ setShowLogin }) => {
     const navigate = useNavigate();
 
     return (
-        <div className='navbar'>
-            <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
-            <ul className="navbar-menu">
+        <div className='Navbar'>
+            <Link to='/'><img src={assets.Navbar_logo} alt="" className="logo" /></Link>
+            <ul className="Navbar-menu">
                 <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
                 <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
                 <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
                 <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>contact us</a>
             </ul>
-            <div className="navbar-right">
+            <div className="Navbar-right">
                 <img src={assets.search_icon} alt="" />
-                <div className="navbar-search-icon">
+                <div className="Navbar-search-icon">
                     <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                     <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
                 </div>
