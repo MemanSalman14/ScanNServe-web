d9f890d450b751c58f45e527b83ae1bd34d0c50f
14Memansalman <stud.2201201080@kpgu.ac.in>
2025-12-12 16:00:51 +0530

    add 5

--- backend_ScanNServe/server.js
+++ backend_ScanNServe/server.js
@@ -61,6 +61,7 @@ app.use("/images",express.static('uploads'))
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/order",orderRouter)
+app.use("/uploads", express.static('uploads'))
 
 // Error handling middleware
 app.use((err, req, res, next) => {
